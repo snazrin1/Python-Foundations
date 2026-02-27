9c81d95e18cedca3f14bf8baf475b5d26a9c4d40
Sheenaz-Salim <snazrin1@gmail.com>
2026-02-27 16:12:22 +0000

    Finalized Calculator

---
+++ calculator.py
@@ -0,0 +1,20 @@
+#int for whole numbers
+#float for decimal numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result_add = num1 + num2
+result_subtract = num1 - num2
+result_multiply = num1 * num2
+result_divide = num1 / num2
+result_modulo = num1 % num2
+
+print(f"The result of addition of {num1} and {num2} is: {result_add}")
+print(f"The result of Subtraction of {num1} and {num2} is: {result_subtract}")
+print(f"The result of Multiplication of {num1} and {num2} is: {result_multiply}")
+print(f"The result of Division of {num1} and {num2} is: {result_divide}")
+print(f"The result of Remindar of {num1} and {num2} is: {result_modulo}")
+
+
+
