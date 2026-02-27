c7f6d093879538e527b98363291699014f29abdc
Sheenaz-Salim <snazrin1@gmail.com>
2026-02-27 15:08:18 +0000

    Completed Story Generator

---
+++ storygenerator.py
@@ -0,0 +1,11 @@
+# Gathering user data
+name = input("Enter a name: ")
+action = input("Enter an action (verb ending in -ing): ")
+place = input("Enter a place: ")
+feeling = input("How are you feeling today? ")
+
+# Creating the story
+story = f"One day, {name} was {action} at the {place}. It made them feel very {feeling}!"
+
+print("\n--- YOUR ADVENTURE STORY ---")
+print(story)
